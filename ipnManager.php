930feda5e86c9c4f7c418781bc5cac2b1f301585
gusgeek <gusgeek@icloud.com>
2019-09-07 10:46:58 -0300

    Updated ipnManager.php.

--- ipnManager.php
+++ ipnManager.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 header("HTTP/1.1 200 OK");
-include('../../framework/db.php'); 
-include('core.php'); 
-    
+include('../../framework/db.php');
+include('core.php');
+
 	$array = explode(",", $configDB['opMercadopago']);
 	$token = $array[2];
 
@@ -11,7 +11,7 @@ include('core.php');
 	$response = responsearray($ipn, $_REQUEST['id'], $token);
 
   	// Estado de pago, puede ver los manejadores de estos aqui https://www.mercadopago.com.ar/developers/es/guides/payments/api/handling-responses
-    	$status = $response['collection']['status']; 
+    	$status = $response['collection']['status'];
 
     // ID de la Transaccion, misma que se recibe en $_GET['id'];
     	$id = $response['collection']['id'];
@@ -26,49 +26,36 @@ include('core.php');
 if (isset($external_reference)) {
 
     if ($method == "recurring_payment") {
-    	
+
     }
 
     if ($method == "pos_payment") {
-    	
+
     }
 
     if ($method == "regular_payment") {
 
      	if ($status == "approved") {
 
-            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas 
-                                    	WHERE (idventa = '".$external_reference."' and mediodepago = '3') or
-                                    	(idventa = '".$external_reference."' and mediodepago = '6') 
-                                   	");
-
-            $dbnvi = mysqli_num_rows($nvidb);
-
-            if ($dbnvi != 0 ) {
-
-                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas 
-                                    SET referenciapagodigital = '".$id."', 
-                                        estado = '1' WHERE idventa = '".$external_reference."'");
-            }      
-
             echo "approved";
+            
         }
 
         if ($status == "in_process") {
 
-            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas 
+            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas
                                     	WHERE (idventa = '".$external_reference."' and mediodepago = '3') or
-                                    	(idventa = '".$external_reference."' and mediodepago = '6') 
+                                    	(idventa = '".$external_reference."' and mediodepago = '6')
                                    	");
 
             $dbnvi = mysqli_num_rows($nvidb);
 
             if ($dbnvi != 0 ) {
 
-                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas 
-                                    SET referenciapagodigital = '".$id."', 
+                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas
+                                    SET referenciapagodigital = '".$id."',
                                         estado = '6' WHERE idventa = '".$external_reference."'");
-            }  
+            }
 
             echo "in_process";
 
@@ -76,19 +63,19 @@ if (isset($external_reference)) {
 
         if ($status == "pending") {
 
-            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas 
+            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas
                                     	WHERE (idventa = '".$external_reference."' and mediodepago = '3') or
-                                    	(idventa = '".$external_reference."' and mediodepago = '6') 
+                                    	(idventa = '".$external_reference."' and mediodepago = '6')
                                    	");
 
             $dbnvi = mysqli_num_rows($nvidb);
 
             if ($dbnvi != 0 ) {
 
-                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas 
-                                    SET referenciapagodigital = '".$id."', 
+                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas
+                                    SET referenciapagodigital = '".$id."',
                                         estado = '3' WHERE idventa = '".$external_reference."'");
-            } 
+            }
 
             echo "pending";
 
@@ -97,47 +84,47 @@ if (isset($external_reference)) {
 
         if ($status == "rejected") {
 
-            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas 
+            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas
                                     	WHERE (idventa = '".$external_reference."' and mediodepago = '3') or
-                                    	(idventa = '".$external_reference."' and mediodepago = '6') 
+                                    	(idventa = '".$external_reference."' and mediodepago = '6')
                                    	");
 
             $dbnvi = mysqli_num_rows($nvidb);
 
             if ($dbnvi != 0 ) {
 
-                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas 
-                                    SET referenciapagodigital = '".$id."', 
+                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas
+                                    SET referenciapagodigital = '".$id."',
                                         estado = '5' WHERE idventa = '".$external_reference."'");
-            } 
+            }
 
             echo "rejected";
-            
+
         }
 
         if ($status == "refunded") {
 
-            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas 
+            $nvidb = mysqli_query($db, "SELECT * FROM ".$nombredb.".sqlventas
                                     	WHERE (idventa = '".$external_reference."' and mediodepago = '3') or
-                                    	(idventa = '".$external_reference."' and mediodepago = '6') 
+                                    	(idventa = '".$external_reference."' and mediodepago = '6')
                                    	");
 
             $dbnvi = mysqli_num_rows($nvidb);
 
             if ($dbnvi != 0 ) {
 
-                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas 
-                                    SET referenciapagodigital = '".$id."', 
+                mysqli_query($db, "UPDATE ".$nombredb.".sqlventas
+                                    SET referenciapagodigital = '".$id."',
                                         estado = '7' WHERE idventa = '".$external_reference."'");
-            } 
+            }
 
             echo "refunded";
-            
+
         }
     }
 
 }
-	
+
 //	file_put_contents('filename.txt', print_r($response, true).PHP_EOL, FILE_APPEND | LOCK_EX);
 
 // echo "<pre>";
