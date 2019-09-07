655488cf93d0a801835edb88aa66d9374854b0f1
gusgeek <gusgeek@icloud.com>
2019-09-07 13:07:25 -0300

    Updated actions.php.

--- actions.php
+++ actions.php
@@ -1,27 +1,27 @@
-<?php 
+<?php
 
 	if (isset($_GET['action'])) {
 
 		if ($_GET['action'] == "ipn") {
-			
-			$response = responsearray($ipn, $_REQUEST['id'], $token);
+
+			$response = responseid($ipn, $_REQUEST['id'], $token);
 
 		}
 
 		if ($_GET['action'] == "categorias") {
-			
+
 			$response = responsesimple($category, $token);
 
 		}
 
 		if ($_GET['action'] == "qrlistado") {
-			
+
 			$response = responsesimple($qrlist, $token);
 
 		}
 
 		if ($_GET['action'] == "qrnuevo") {
-			
+
 			$response = post($qrlist, $_REQUEST['array'], $token);
 
 		}
@@ -35,34 +35,34 @@
 		}
 
 		if ($_GET['action'] == "pointlist") {
-			
+
 			$response = responsesimple($devicesPoint, $token);
 
 		}
 
 		if ($_GET['action'] == "pointactive") {
-			
+
 			$response = post($pointActive, $_REQUEST['array'], $token);
 
 		}
 
 		if ($_GET['action'] == "pointdelete") {
-			
+
 			$response = deletePoint($pointDelete, $_REQUEST['device_name'], $token);
 
 		}
 
 		if ($_GET['action'] == "hlink") {
-			
+
 			$response = post($link, $_REQUEST['array'], $token);
 
 		}
 
 		if ($_GET['action'] == "nplan") {
-			
+
 			$response = post($suscriptionscreate, $_REQUEST['array'], $token);
 
 		}
 	}
-	
+
  ?>
