dfa4e1ce8f26721073b1aaec6f43a85b0afff9c0
Agustin Fernandez <38817071+gusgeek@users.noreply.github.com>
2021-01-11 10:07:25 -0300

    Update core.php

--- library/core.php
+++ library/core.php
@@ -114,7 +114,6 @@ namespace SAPIDK_MP {
 
 		public static function Categories() { return CURL::CURL_GET_OT("https://api.mercadopago.com/item_categories"); }
 		public static function Currencies() { return CURL::CURL_GET_OT("https://api.mercadopago.com/currencies"); }
-		public static function CardProm() { return CURL::CURL_GET_OT("https://www.mercadopago.com/mla/credit_card_promos.json"); }
 		public static function IDTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/identification_types", $token); }
 		public static function MethodTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payment_methods", $token); }
 
