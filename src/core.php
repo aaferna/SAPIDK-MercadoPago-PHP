e98fe1b173a14b2670805cd2a2a0a2eb4b69d809
Agustin Fernandez <aaferna@outlook.com.ar>
2021-03-06 00:24:59 -0300

    composer

--- src/core.php
+++ src/core.php
@@ -1,10 +1,10 @@
 <?php 
 
-namespace SAPIDK_MP;
+namespace SAPIDKMP;
 
 	class CURL {
 
-		public static function CURL_POST($uri, $array, $token) {
+		public function CURL_POST($uri, $array, $token) {
 
 			$url = $uri."?access_token=".$token;
 
@@ -28,7 +28,7 @@ namespace SAPIDK_MP;
 
 		}
 
-		public static function CURL_GET_OA($uri, $token) {
+		public function CURL_GET_OA($uri, $token) {
 
 			$url = $uri."?access_token=".$token;
 
@@ -44,7 +44,7 @@ namespace SAPIDK_MP;
 
 		}
 
-		public static function CURL_GET_OT($url) {
+		public function CURL_GET_OT($url) {
 
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -58,7 +58,7 @@ namespace SAPIDK_MP;
 
 		}
 
-		public static function CURL_GET_AB($uri, $token) {
+		public function CURL_GET_AB($uri, $token) {
 
 			$curl = curl_init();
 
@@ -85,7 +85,7 @@ namespace SAPIDK_MP;
 		}
 
 
-		public static function CURL_DELETE_AB($uri, $token) {
+		public function CURL_DELETE_AB($uri, $token) {
 
 			$url = $uri."?access_token=".$token;
 			$ch = curl_init();
@@ -112,46 +112,46 @@ namespace SAPIDK_MP;
 
 	class Action {
 
-		public static function Categories() { return CURL::CURL_GET_OT("https://api.mercadopago.com/item_categories"); }
-		public static function Currencies() { return CURL::CURL_GET_OT("https://api.mercadopago.com/currencies"); }
-		public static function IDTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/identification_types", $token); }
-		public static function MethodTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payment_methods", $token); }
+		public function Categories() { return CURL::CURL_GET_OT("https://api.mercadopago.com/item_categories"); }
+		public function Currencies() { return CURL::CURL_GET_OT("https://api.mercadopago.com/currencies"); }
+		public function IDTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/identification_types", $token); }
+		public function MethodTypes($token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payment_methods", $token); }
 
-		public static function NewBox($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/pos", $data, $token); }
-		public static function GetBox($token) { return CURL::CURL_GET_AB("https://api.mercadopago.com/pos", $token); }
+		public function NewBox($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/pos", $data, $token); }
+		public function GetBox($token) { return CURL::CURL_GET_AB("https://api.mercadopago.com/pos", $token); }
 
-		public static function Link($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/checkout/preferences", $data, $token); }
-		public static function QRActive($data, $cred, $token) { return CURL::CURL_POST("https://api.mercadopago.com/mpmobile/instore/qr/".$cred['user_id']."/".$cred['external_id'], $data, $token); }
+		public function Link($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/checkout/preferences", $data, $token); }
+		public function QRActive($data, $cred, $token) { return CURL::CURL_POST("https://api.mercadopago.com/mpmobile/instore/qr/".$cred['user_id']."/".$cred['external_id'], $data, $token); }
 
-		public static function PointList($token) { return CURL::CURL_GET_OA("https://mobile.mercadopago.com/point/services/integrations/v1/devices", $token); }
-		public static function PointActive($data, $token) { return CURL::CURL_POST("https://mobile.mercadopago.com/point/services/integrations/v1", $data, $token); }
-		public static function PointDelete($device, $token) { return CURL::CURL_DELETE_AB("https://mobile.mercadopago.com/point/services/integrations/v1/attempt/device/".$device, $token); }
+		public function PointList($token) { return CURL::CURL_GET_OA("https://mobile.mercadopago.com/point/services/integrations/v1/devices", $token); }
+		public function PointActive($data, $token) { return CURL::CURL_POST("https://mobile.mercadopago.com/point/services/integrations/v1", $data, $token); }
+		public function PointDelete($device, $token) { return CURL::CURL_DELETE_AB("https://mobile.mercadopago.com/point/services/integrations/v1/attempt/device/".$device, $token); }
 
-		public static function IPN($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/collections/notifications/".$id, $token); }
+		public function IPN($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/collections/notifications/".$id, $token); }
 
-		public static function PaymentView($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payments/".$id, $token); }
-		public static function PaymentRefound($id, $token) { return CURL::CURL_POST("https://api.mercadopago.com/v1/payments/".$id."/refunds", "", $token); }
-		public static function PaymentList($limit, $offset, $token) { 
+		public function PaymentView($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payments/".$id, $token); }
+		public function PaymentRefound($id, $token) { return CURL::CURL_POST("https://api.mercadopago.com/v1/payments/".$id."/refunds", "", $token); }
+		public function PaymentList($limit, $offset, $token) { 
 
 		 	$ext = "limit=".$limit."&offset=".$offset;
 			return CURL::CURL_GET_OA("https://api.mercadopago.com/v1/payments/search", $token."&".$ext); 
 
 		}
 
-		public static function UserView($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/users/".$id, $token); }
+		public function UserView($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/users/".$id, $token); }
 
 
-		public static function StoreID($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/stores/".$id, $token); }
-		public static function StoreNew($data, $id, $token) { return CURL::CURL_POST("https://api.mercadopago.com/users/".$id."/stores", $data, $token); }
-		public static function StoreDelete($idsucursal, $id, $token) { return CURL::CURL_DELETE_AB("https://api.mercadopago.com/users/".$id."/stores/".$idsucursal, $token); }
-		public static function StoreList($id, $limit, $offset, $token) { 
+		public function StoreID($id, $token) { return CURL::CURL_GET_OA("https://api.mercadopago.com/stores/".$id, $token); }
+		public function StoreNew($data, $id, $token) { return CURL::CURL_POST("https://api.mercadopago.com/users/".$id."/stores", $data, $token); }
+		public function StoreDelete($idsucursal, $id, $token) { return CURL::CURL_DELETE_AB("https://api.mercadopago.com/users/".$id."/stores/".$idsucursal, $token); }
+		public function StoreList($id, $limit, $offset, $token) { 
 
 			$ext = "limit=".$limit."&offset=".$offset;
 			return CURL::CURL_GET_OA("https://api.mercadopago.com/users/".$id."/stores/search", $token."&".$ext); 
 
 		}
 		
-		public static function RecurrentCreator($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/preapproval", $data, $token); }
+		public function RecurrentCreator($data, $token) { return CURL::CURL_POST("https://api.mercadopago.com/preapproval", $data, $token); }
 
 
 
